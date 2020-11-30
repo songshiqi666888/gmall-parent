c49a319640ddfa065113a903204df4a4601e46a1
songshiqi <403395210@qq.com>
2020-12-01 01:07:18 +0800

    查询分级属性和属性值

--- service/service-product/src/main/java/com/atguigu/gmall1213/product/mapper/BaseAttrInfoMapper.java
+++ service/service-product/src/main/java/com/atguigu/gmall1213/product/mapper/BaseAttrInfoMapper.java
@@ -3,7 +3,13 @@ package com.atguigu.gmall1213.product.mapper;
 import com.atguigu.gmall.model.product.BaseAttrInfo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface BaseAttrInfoMapper extends BaseMapper<BaseAttrInfo> {
+    List<BaseAttrInfo> selectBaseAttrInfoList(@Param("category1Id")long category1Id,
+                                              @Param("category2Id")long category2Id,
+                                              @Param("category3Id")long category3Id);
 }
